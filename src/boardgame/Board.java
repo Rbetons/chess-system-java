a091fa266be3ea27760ada4dfaaef8b53c238e7a
Roberto Nunes <rbetons@gmail.com>
2020-04-03 20:26:32 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,18 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao fora do tabuleiro");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;		
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -58,6 +70,8 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
+	
 	
 	
 	
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,7 +9,7 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, int row) {
 		if (column < 'a'|| column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Erro ao instanciar o ChessPosition. Os valores validos sao de a1 a h8");
+			throw new ChessException("Erro ao instanciar a posicao do xadrez. Os valores validos sao de a1 a h8");
 		}
 		this.column = column;
 		this.row = row;
